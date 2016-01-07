133c8029a20d0eedb66bc78d22af845f6921c186
Baljit <baljit.singh@suncorp.co.nz>
2016-01-08 09:00:00 +1300

    Initial commit

---
+++ src/main/webapp/scripts/app.js
@@ -0,0 +1,37 @@
+/**
+ * Created by shekhargulati on 10/06/14.
+ */
+
+var app = angular.module('todoapp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+]);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'views/list.html',
+        controller: 'ListCtrl'
+    }).otherwise({
+        redirectTo: '/'
+    })
+});
+
+app.controller('ListCtrl', function ($scope, $http, $filter) {
+
+    var orderBy = $filter('orderBy');
+
+    $http.get('http://localhost:7090/case-service/getAll').success(function (data) {
+        $scope.todos = data;
+
+        $scope.order = function (predicate, reverse) {
+            $scope.todos = orderBy($scope.todos, predicate, reverse);
+        };
+
+    }).error(function (data, status) {
+        console.log('Error ' + data)
+    })
+
+});
